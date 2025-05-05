9402b160cd35d4a1c8a1f296950c03e40bcc7fb3
Isaiah Vickers <isaiah.vickers@outlook.com>
2025-05-05 07:14:22 -0400

    initialize backend

---
+++ backend/src/config/index.ts
@@ -0,0 +1,11 @@
+const config = {
+	port: process.env.PORT,
+	auth: {
+		sessionSecret: process.env.SESSION_SECRET,
+		google: {
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			apiKey: process.env.GOOGLE_CLIENT_SECRET,
+		},
+	},
+	frontendUrl: process.env.FRONTEND_URL,
+};
