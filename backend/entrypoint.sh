6cfe97b22cca2943bd71928a0642e3a168e42e57
Isaiah Vickers <isaiah.vickers@outlook.com>
2025-05-12 13:54:33 -0400

    modify backend dockerfile; add healthcheck and update routes

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 
 # Run database migrations
 echo "Running Prisma migration..."
-pnpx prisma migrate deploy
+pnpx prisma migrate deploy || { echo "Migration failed"; exit 1; }
 
 # Start server
 echo "Starting application"
-exec pnpm start
+exec pnpm start || { echo "App start failed"; exit 1; }
