bdae14ebf082a67d6a080b3aebd5e2f8e6f47c3a
Isaiah Vickers <isaiah.vickers@outlook.com>
2025-05-29 15:35:11 -0400

    add client model; add user session to sessions schema
    - Prisma kept asking to reset the migration because of the hidden table that was under public, so I moved auth sessions to a new schema

--- apps/backend/prisma/migrations/20250529192006_add_client_model/migration.sql
+++ apps/backend/prisma/migrations/20250529192006_add_client_model/migration.sql
@@ -4,7 +4,6 @@
   - The `status` column on the `Subscription` table would be dropped and recreated. This will lead to data loss if there is data in the column.
   - You are about to drop the column `paymentMethod` on the `Transaction` table. All the data in the column will be lost.
   - You are about to drop the column `currentSubscription` on the `User` table. All the data in the column will be lost.
-  - You are about to drop the `user_sessions` table. If the table is not empty, all the data it contains will be lost.
   - Changed the type of `type` on the `Transaction` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
 
 */
@@ -20,9 +19,6 @@ ADD COLUMN     "type" TEXT NOT NULL;
 -- AlterTable
 ALTER TABLE "User" DROP COLUMN "currentSubscription";
 
--- DropTable
-DROP TABLE "user_sessions";
-
 -- DropEnum
 DROP TYPE "SubscriptionPlan";
 
@@ -34,3 +30,23 @@ DROP TYPE "TransactionPaymentMethod";
 
 -- DropEnum
 DROP TYPE "TransactionType";
+
+-- CreateTable
+CREATE TABLE "Client" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "projectId" TEXT NOT NULL,
+    "addedBy" TEXT NOT NULL,
+
+    CONSTRAINT "Client_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Client_email_projectId_addedBy_key" ON "Client"("email", "projectId", "addedBy");
+
+-- AddForeignKey
+ALTER TABLE "Client" ADD CONSTRAINT "Client_addedBy_fkey" FOREIGN KEY ("addedBy") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Client" ADD CONSTRAINT "Client_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
