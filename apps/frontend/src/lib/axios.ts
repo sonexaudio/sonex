408d9abaab1b62cdf94f5da8426df22d115c52e5
Isaiah Vickers <isaiah.vickers@outlook.com>
2025-07-30 05:52:48 -0400

    update axios instance

--- apps/frontend/src/lib/axios.ts
+++ apps/frontend/src/lib/axios.ts
@@ -5,23 +5,4 @@ const api = axios.create({
 	withCredentials: true,
 });
 
-// Check for a client token in the cookies and set it as bearer token if it exists
-api.interceptors.request.use((config) => {
-	const clientToken = localStorage.getItem("clientAccess");
-	if (clientToken) {
-		console.log("Setting client token for API requests");
-		config.headers = config.headers || {};
-		config.headers.Authorization = `Bearer ${clientToken}`;
-	}
-	return config;
-});
-
-// Handle logout and clear client token
-api.interceptors.response.use((response) => {
-	if (response.data?.logout) {
-		localStorage.removeItem("clientAccess");
-	}
-	return response;
-});
-
 export default api;
